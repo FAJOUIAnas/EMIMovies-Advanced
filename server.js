b797ed4303d9835a97b772c1369648883ccff922
ouhaddaOualid <83510523+ouhaddaOualid@users.noreply.github.com>
2022-11-25 21:33:28 +0100

    last one 5

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/projets'));
+
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+ '/dist/projets/index.html'));});
+
+  res.sendFile(path.join(__dirname+'/dist/projets/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
